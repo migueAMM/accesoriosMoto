a80ad07d3bc2addbfd8bc0b109f327d5546a6910
Miguel Angel Marin Montes <mamarinmontes@gmail.com>
2026-09-07 10:04:55 -0500

    Se ha creado la estructura inicial de las carpetas del proyecto y se ha conectado a un repositorio

---
+++ src/main/java/com/uniquindio/accesoriosMoto/AccesoriosMotoApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.accesoriosMoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoriosMotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoriosMotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/accesoriosMoto/AccesoriosMotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.accesoriosMoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoriosMotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
