1231e094c18037f14776f24e0b2992a9efafc3bd
Juan Suarez <juanj.suarezm@uqvirtual.edu.co>
2026-09-12 15:54:56 -0500

    feat: creacion clase ReglaDominioException

--- src/main/java/com/uniquindio/accesoriosMoto/domain/exception/ReglaDominioException.java
+++ src/main/java/com/uniquindio/accesoriosMoto/domain/exception/ReglaDominioException.java
@@ -1,5 +1,7 @@
 package com.uniquindio.accesoriosMoto.domain.exception;
 
 public class ReglaDominioException extends RuntimeException{
-
+    public ReglaDominioException(String mensaje) {
+        super(mensaje);
+    }
 }
