debf97993536cda111d98d43f759e5bd0d4766a7
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2026-09-12 17:21:31 -0500

    feat: creacion enum meterial

---
+++ src/main/java/com/uniquindio/accesoriosMoto/domain/valueObject/Material.java
@@ -0,0 +1,23 @@
+package com.uniquindio.accesoriosMoto.domain.valueObject;
+
+public enum Material {
+    POLICARBONATO,
+    ABS,
+    FIBRA_DE_VIDRIO,
+    FIBRA_DE_CARBONO,
+    KEVLAR,
+    CUERO,
+    CUERO_SINTETICO,
+    TEXTIL,
+    NYLON,
+    POLIESTER,
+    GOMA,
+    SILICONA,
+    ALUMINIO,
+    ACERO,
+    ACERO_INOXIDABLE,
+    PLASTICO,
+    PVC,
+    ACRILICO,
+    CAUCHO;
+}
