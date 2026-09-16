3e488150d12a63c74ce3198a6a834adc73a1d09d
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2026-09-16 17:45:14 -0500

    fix: se arreglo atributos que faltaban en casco

--- src/main/java/com/uniquindio/accesoriosMoto/domain/entity/Casco.java
+++ src/main/java/com/uniquindio/accesoriosMoto/domain/entity/Casco.java
@@ -2,6 +2,7 @@ package com.uniquindio.accesoriosMoto.domain.entity;
 
 import com.uniquindio.accesoriosMoto.domain.valueObject.Marca;
 import com.uniquindio.accesoriosMoto.domain.valueObject.Precio;
+import com.uniquindio.accesoriosMoto.domain.valueObject.Proteccion;
 import com.uniquindio.accesoriosMoto.domain.valueObject.Talla;
 
 import java.util.Objects;
@@ -13,8 +14,9 @@ public class Casco {
     private String color;
     private Talla talla;
     private Precio precio;
+    private Proteccion proteccion;
 
-    private Casco(String id, Marca marca, String modelo, String color, Talla talla, Precio precio) {
+    private Casco(String id, Marca marca, String modelo, String color, Talla talla, Precio precio, Proteccion proteccion) {
         this.id = id;
         this.marca = marca;
         this.modelo = modelo;
@@ -22,10 +24,13 @@ public class Casco {
         this.talla = talla;
         this.precio = precio;
     }
+    public Proteccion getProteccion() {
+        return proteccion;
+    }
 
 
-   public static Casco crear(String id, Marca marca, String modelo, String color, Talla talla, Precio precio) {
-       return new Casco(id, marca, modelo, color, talla, precio);
+   public static Casco crear(String id, Marca marca, String modelo, String color, Talla talla, Precio precio, Proteccion proteccion) {
+       return new Casco(id, marca, modelo, color, talla, precio, proteccion);
    }
 
    @Override
