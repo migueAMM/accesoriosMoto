a1f8e920ea55c8e9ed6618aa41878fbc67c8530d
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2026-09-17 12:17:12 -0500

    feat: se creo la clase padre Usuario con sus clases hijas y unas clases relacionadas con los pedidos

---
+++ src/main/java/com/uniquindio/accesoriosMoto/domain/entity/Pedido.java
@@ -0,0 +1,39 @@
+package com.uniquindio.accesoriosMoto.domain.entity;
+
+import com.uniquindio.accesoriosMoto.domain.valueObject.EstadoPedido;
+import com.uniquindio.accesoriosMoto.domain.valueObject.Precio;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class Pedido {
+
+    private final String id;
+    private Comprador comprador;
+    private List<Producto> productos;
+    private EstadoPedido estadoPedido;
+    private LocalDateTime fecha;
+    private Precio total;
+
+    public Pedido(String id, Comprador comprador, List<Producto> productos, EstadoPedido estadoPedido, LocalDateTime fecha, Precio total) {
+        this.id = id;
+        this.comprador = comprador;
+        this.productos = productos;
+        this.estadoPedido = estadoPedido;
+        this.fecha = fecha;
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pedido)) return false;
+        return id.equals(((Pedido) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/uniquindio/accesoriosMoto/domain/entity/Usuario.java
@@ -0,0 +1,30 @@
+package com.uniquindio.accesoriosMoto.domain.entity;
+
+import java.util.Objects;
+
+public abstract class Usuario {
+    protected final String id;
+    protected String nombre;
+    protected String correo;
+    protected String contrasena;
+
+    public Usuario(String id, String nombre, String correo, String contrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
