244df57d248730a3813839a760dd79f5c6caa2af
Miguel Angel Marin Montes <mamarinmontes@gmail.com>
2026-09-17 09:43:52 -0500

    Se ha corregido un error en la clase precio y se agrego la estructura de los test para las pruebas unitarias

---
+++ src/test/java/com/uniquindio/accesoriosMoto/domain/valueobject/PrecioTest.java
@@ -0,0 +1,18 @@
+package com.uniquindio.accesoriosMoto.domain.valueobject;
+
+import com.uniquindio.accesoriosMoto.domain.valueObject.Precio;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.uniquindio.accesoriosMoto.domain.exception.ReglaDominioException;
+
+
+public class PrecioTest {
+
+    @Test
+    void dosPreciosConElMismoValorDebenSerIguales(){
+        Precio p1 = new Precio(15000 , "COP");
+        Precio p2 = new Precio(15000 , "COP");
+        assertEquals(p1 , p2);
+    }
+
+}
